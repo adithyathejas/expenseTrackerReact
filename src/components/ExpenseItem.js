21e079e54950826e05103633d2344d9a246c8c24
adithyathejas <adithyathejus00@gmail.com>
2022-10-09 23:30:32 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){
+    return (<div>
+            <h2>Expense item!</h2>
+            <i>Food Rs 10</i>
+            <p>Petrol Rs 100</p>
+            <p>Movies Rs 200</p>
+    </div>
+        
+);
+
+}
+
+export  default ExpenseItem
