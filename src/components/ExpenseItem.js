11f7a1f92af54c3d65817757c2d864b544fb76af
adithyathejas <adithyathejus00@gmail.com>
2022-10-12 23:24:44 +0530

    adding elements

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,15 @@
 import "./ExpenseItem.css"
 
-function ExpenseItem(){
-
-    const expenseDate = new Date(2021,2,28)
-    const expenseTitle = 'Car Insurance '
-    const expenseAmount = 294.67
-    const LocationOfExpenditure = 'LIC KOLLAM BRANCH'
+function ExpenseItem(props){
     return (
             <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{'   '}{props.date.toISOString()}</div>
             <div classNmae="expense-item__description">
-            <h2>{expenseTitle}</h2>
-            <div className="expense-item__price">${expenseAmount}</div>
+            <h2>{'   '}{props.title}</h2>
+            <div className="expense-item__price">{'   '}{props.amount}</div>
             </div> 
-            <br></br> 
-            <div className="expense-item__description">spend on:<br></br>  {LocationOfExpenditure}  </div>
+             
+            <div className="expense-item__description">spend on:<br></br>  {'   '}{props.LocationOfExpenditure}  </div>
             </div>                 
 );
 
