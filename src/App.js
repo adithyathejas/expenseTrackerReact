65e8268f44d7017494787df94045f9cbc6600ef8
adithyathejas <adithyathejus00@gmail.com>
2022-10-13 08:42:39 +0530

    looping done

--- src/App.js
+++ src/App.js
@@ -25,19 +25,19 @@ function App() {
       LocationOfExpenditure : 'ANILKUMAR FURNITURES, TVM'
     },
   ];
-  return (
+ const array=[]
+  for(let i=0;i<expenses.length;i++){
+    array.push(<ExpenseItem title={expenses[i].title} amount={expenses[i].amount} date={expenses[i].date} LocationOfExpenditure = {expenses[i].LocationOfExpenditure}  ></ExpenseItem>)
+ }
+    return (
     
-    <div>
-      <h2>Let's get started!</h2>
-         <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} LocationOfExpenditure = {expenses[0].LocationOfExpenditure}  ></ExpenseItem>
-         <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}   LocationOfExpenditure = {expenses[1].LocationOfExpenditure}></ExpenseItem>
-         <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}  LocationOfExpenditure = {expenses[2].LocationOfExpenditure} ></ExpenseItem>
-         <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}  LocationOfExpenditure = {expenses[3].LocationOfExpenditure} ></ExpenseItem>
-         
-      
-      
-    </div>
-  );
-}
+      <div>
+        <h2>Let's get started!</h2>
+        {array} 
+      </div>
+    );
+  }
+  
+
 
 export default App;
