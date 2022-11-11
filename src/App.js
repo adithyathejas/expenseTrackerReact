fb1d523e3e0ea2f5b422d38aaa3c499159176597
adithyathejas <adithyathejus00@gmail.com>
2022-11-11 11:47:32 +0530

    showed component on ui

--- src/App.js
+++ src/App.js
@@ -1,51 +1,61 @@
 
+import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
+import { useState } from "react";
+import Expenses from "./components/Expenses/Expenses"
 
-import ExpenseItem from "./components/Expenses/ExpenseItem";
-import Card from "./components/UI/Card"
-import ExpenseForm from "./components/ExpenseForm/ExpenseForm"
-
-
-
-const App=()=> {
+const App = () => {
   const expenses = [
     {
-      id: 'e1',
-      title: 'Toilet Paper',
+      id: "e1",
+      title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      LocationOfExpenditure : 'Dhanya supermarket'
+      LocationOfExpenditure: "Dhanya supermarket",
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12),
-     LocationOfExpenditure : 'Samsung Electronics'},
     {
-      id: 'e3',
-      title: 'Car Insurance',
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      LocationOfExpenditure: "Samsung Electronics",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
       amount: 294.67,
-      date: new Date(2021, 2, 28),LocationOfExpenditure : 'LIC Kollam BRANCH'
+      date: new Date(2021, 2, 28),
+      LocationOfExpenditure: "LIC Kollam BRANCH",
     },
     {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
+      id: "e4",
+      title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
-      LocationOfExpenditure : 'ANI FURNITURES, TVM'
+      LocationOfExpenditure: "ANI FURNITURES, TVM",
     },
   ];
- const array=[]
-  for(let i=0;i<expenses.length;i++){
-    array.push(<ExpenseItem title={expenses[i].title} amount={expenses[i].amount} date={expenses[i].date} LocationOfExpenditure = {expenses[i].LocationOfExpenditure}  ></ExpenseItem>)
- }
-    return (<Card>
-           <ExpenseForm></ExpenseForm>
-           <div id="root">
-           {array} 
-           </div>
-      
-    </Card>
-       
-    );
-  }
-  
+ 
 
+  const [expense,setExpense] = useState(expenses)
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const enteredExpense = {
+      ...enteredExpenseData,id:Math.random()
+    }
+   
+    setExpense((prevState)=>{
+      return [...prevState,enteredExpenseData]
+
+    })
+  
+  };
+
+  return (
+    <div>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}></ExpenseForm>
+      <Expenses expenseValue={expense}></Expenses>
+    </div>
+  );
+};
 
 export default App;
